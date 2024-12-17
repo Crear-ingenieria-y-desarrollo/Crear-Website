aabc8898ca181db989e0dc10e46f85ba284f1434
SebastianCP <sebastian.cardozo@neotech.co>
2024-12-16 23:01:21 -0500

    Creacion pagina web astro

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgba(39,118,209,1)',
+        'primary-dark': 'rgba(31,94,167,1)',
+      },
+      animation: {
+        'float': 'float 3s ease-in-out infinite',
+        'fadeIn': 'fadeIn 1.5s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
